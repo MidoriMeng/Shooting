454705f2744d028ab4ee32695d3b424f6da8b364
MidoriMeng <1193667040@qq.com>
2017-08-23 18:51:40 +0800

    移动控制+镜头（第一/第三）

---
+++ Assets/Scripts/Level/CameraSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour {
+    public GameObject firstCamera;
+    public GameObject thirdCamera;
+
+    private bool _isFirstPerson;
+
+    public bool isFirstPerson {
+        get { return _isFirstPerson; }
+        set {
+            _isFirstPerson = value;
+            firstCamera.SetActive(_isFirstPerson);
+            thirdCamera.SetActive(!_isFirstPerson);
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Level/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed = 5f;
+    /// <summary>
+    /// 倍速
+    /// </summary>
+    public float accelerate = 2f;
+
+    Vector3 movement = Vector3.zero;
+    Rigidbody rb;
+    // Use this for initialization
+    void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        Move();
+    }
+    void Move() {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        movement.y = 0;
+        Debug.Log(movement);
+        movement = movement.normalized * speed * Time.deltaTime;
+        if (Input.GetButton("Accelerate"))
+            movement *= 2f;
+        Vector3 newPos = transform.position + movement;
+        rb.MovePosition(transform.position + movement.z * transform.forward + movement.x * transform.right + movement.y * transform.up);
+    }
+}
