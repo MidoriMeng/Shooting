0e66baeb23b008cbfcafbda7c22101d8b818d664
MidoriMeng <1193667040@qq.com>
2017-08-23 13:56:00 +0800

    实验：场景中角色密度

---
+++ Assets/Scripts/AddObjects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObjects : MonoBehaviour {
+    public GameObject[] prefabs;
+    public float mapSize = 3f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddObjRandomly() {
+        int index = (int)Random.Range(0, prefabs.Length - 0.001f);
+        GameObject obj = Instantiate<GameObject>(prefabs[index]);
+        obj.transform.parent = transform;
+
+        float range = mapSize * 10f / 2f;
+        Vector3 pos = new Vector3(
+            Random.Range(-range, range),
+            1.1f,
+            Random.Range(-range, range)
+            );
+        obj.transform.localPosition = pos;
+        if (index > 1) {
+            obj.transform.localRotation = Quaternion.identity;
+        } else {
+            obj.transform.localRotation = Random.rotation;
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+    private int curPlayerScore = 0;
+
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NewGame() {
+        curPlayerScore = 0;
+    }
+
+    public void AddScore(int add) {
+        curPlayerScore += add;
+    }
+
+
+}
