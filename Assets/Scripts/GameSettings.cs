8cdf0c9da5d682af02a821e9c68b4e2beeb1b650
MidoriMeng <1193667040@qq.com>
2017-08-23 15:48:51 +0800

    +简单的欢迎页面

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameSettings : MonoBehaviour {
     private int curPlayerScore = 0;
-
-	void Awake () {
+    public const int MAX_HIGHSCORE = 10;
+    public static string HIGHSCORENUMBER = "HightScore";
+    public static string HIGHSCORENAME = "HighScoreName";
+    void Awake() {
         DontDestroyOnLoad(gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        for (int i = 0; i < MAX_HIGHSCORE; i++) {
+            if (!PlayerPrefs.HasKey(HIGHSCORENUMBER + i)) {
+                PlayerPrefs.SetFloat(HIGHSCORENUMBER + i, 0);
+                PlayerPrefs.SetString(HIGHSCORENAME + i, string.Empty);
+            }
+        }
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     public void NewGame() {
         curPlayerScore = 0;
@@ -22,5 +31,11 @@ public class GameSettings : MonoBehaviour {
         curPlayerScore += add;
     }
 
+    public void QuitGame() {
+        Application.Quit();
+    }
 
+    public void LoadLevel(int level) {
+        SceneManager.LoadScene(level);
+    }
 }
---
+++ Assets/Scripts/WelcomePage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WelcomePage : MonoBehaviour {
+    public GameObject highScore;
+    public Text[] highScores;
+    public void Exit() {
+        Singleton<GameSettings>.Instance.QuitGame();
+    }
+
+    public void NewGame() {
+        Singleton<GameSettings>.Instance.LoadLevel(1);
+    }
+
+    public void OpenHighScore() {
+        highScore.SetActive(true);
+        for(int i = 0; i < highScores.Length; i++) {
+            float score = PlayerPrefs.GetFloat(GameSettings.HIGHSCORENUMBER + i);
+            string name = PlayerPrefs.GetString(GameSettings.HIGHSCORENAME + i);
+            highScores[i].text = (i + 1) + ".  " + name + "     " + score;
+        }
+    }
+
+    public void CloseHighScore() {
+        highScore.SetActive(false);
+    }
+}
