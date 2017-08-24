8fe93c375c1bffc48408a51ca941c3892f30e397
MidoriMeng <1193667040@qq.com>
2017-08-24 15:20:13 +0800

    AI状态机结构确定：Action + Mood + Personality；调整测试的动画

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,43 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    public float Speed;
+    float speedPercentage;
     float health;
     float attack;
 
     public enum EnemyStateEnum {
-        Attack, Idle
+        Idle, Move, Attack
     };
+
+    public enum Mood {
+        Normal, Angry, Frightened
+    }
+
+    public enum Personality {
+        EatMelon, Evil, Coward
+    }
+
     EnemyState curState;
+    private Mood curMood;
+    Personality personality;
 
     void Awake() {
+        personality = (Personality)(int)Random.Range(0, 2.999f);//性格在这里随机赋值？
+        Debug.Log(personality);
         curState = new IdleState(this);
         curState.Enter();
+
     }
 
     void Update() {
-        EnemyState newState = curState.CheckTransition() as EnemyState;
-        if(newState.type != curState.type) {
+        RunStateMachine();
+        MoodTransition();
+    }
+
+    void MoodTransition() {
+        //curMood
+    }
+
+
+    //-------------------------STATE----------------------------------
+    void RunStateMachine() {
+        EnemyState newState = curState.CheckTransition();
+        if (newState.type != curState.type) {
             curState.Exit();
             curState = newState;
             curState.Enter();
         }
-	}
+        curState.Update();
+    }
 
-    
     public class IdleState : EnemyState {
-        Enemy context;
 
         public IdleState(Enemy enemy) { context = enemy; type = EnemyStateEnum.Idle; }
-        public override void Enter() { }
-        public override void Exit() { }
-        public override void Update() { }
-        public override State CheckTransition() {
+        public override void Enter() {
+        }
+        public override void Exit() {
+        }
+        public override void Update() {
+        }
+        public override EnemyState CheckTransition() {
+            if (context.speedPercentage > 0.1f)
+                return new MoveState(context);
             return this;
         }
     }
 
-    public class EnemyState : State {
-        public EnemyStateEnum type;
+    public class MoveState : EnemyState {
+
+        public MoveState(Enemy enemy) { context = enemy; type = EnemyStateEnum.Move; }
+        public override void Enter() {
+        }
+        public override void Exit() {
+        }
+        public override void Update() {
+        }
+        public override EnemyState CheckTransition() {
+            if (context.speedPercentage < 0.1f)
+                return new IdleState(context);
+            return this;
+        }
     }
+
+    public class EnemyState {
+        public EnemyStateEnum type;
+        protected Enemy context;
+
+        public virtual void Enter() { }
+        public virtual void Exit() { }
+        public virtual void Update() { }
+        public virtual EnemyState CheckTransition() { return this; }
+    }      
 }
 
