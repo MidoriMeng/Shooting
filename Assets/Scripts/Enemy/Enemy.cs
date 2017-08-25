b320273bf6935be08254183bdbefbdd1fdb8e734
MidoriMeng <1193667040@qq.com>
2017-08-25 15:12:50 +0800

    修改AI设计，暂时删除任务

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
-    public bool use = false;
     public float Speed = 3f;
-    public Transform goodWaitPos;
+    public float attackDistance = 1f;
+    public float alarmDistance = 7f;
+    public float alarmHPPercent;
+    /*public Transform goodWaitPos;
     public Transform badWaitPos;
-    public Transform carPos;
-    float health;
+    public Transform carPos;*/
+    public float curHealth;
+    public float maxHealth;
     float attack;
+    float playerDistance = 0;
 
     EnemyState curState;
-    Mood curMood;
+    //Mood curMood;
     Personality personality;
-    public Task curTask = Task.NormalWaitForCar;
-    TaskCompletion completion;
+    //public Task curTask = Task.NormalWaitForCar;
+    //TaskCompletion completion;
 
     NavMeshAgent agent;
     Animator anim;
@@ -25,54 +29,61 @@ public class Enemy : MonoBehaviour {
         personality = (Personality)(int)Random.Range(0, 2.999f);//性格在这里随机赋值？
         Debug.Log(personality);
         curState = new IdleState(this);
-        curState.Enter();
+
         personality = Personality.Evil;
-        completion = TaskCompletion.NotStarted;
+        //completion = TaskCompletion.NotStarted;
 
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
     }
 
+    void Start() {
+        curState.Enter();
+    }
+
     void Update() {
         //决策是否搞事(curTask)
+
+        //更新数值
         anim.SetFloat("Speed", agent.velocity.magnitude / agent.speed);
+        playerDistance = Vector3.Distance(Singleton<Player>.Instance.transform.position, transform.position);
+
+        //状态机更新
         RunStateMachine();
         MoodTransition();
-
-        if (use)
-            AssignTask(Task.ComeUp);
     }
 
     void MoodTransition() {
         //curMood
     }
 
-    void FreezeAll(bool freeze) {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.constraints = freeze? RigidbodyConstraints.FreezeAll: RigidbodyConstraints.None;
-    }
     /// <summary>
     /// 设定agent目标到车门附近，根据是否守规矩决定具体位置
     /// </summary>
     /// <param name="isGood"></param>
-    void WaitForCar(bool isGood) {
-        FreezeAll(false);
-        agent.SetDestination(isGood ? goodWaitPos.position : badWaitPos.position);
+    /* void WaitForCar(bool isGood) {
+         agent.enabled = true;
+         agent.SetDestination(isGood ? goodWaitPos.position : badWaitPos.position);
+     }
+
+     void ComeUp() {
+         agent.enabled = true;
+         agent.SetDestination(carPos.position);
+     }
+
+     public void AssignTask(Task newTask) {
+         curTask = newTask;
+         completion = TaskCompletion.NotStarted;
+     }*/
+
+    public void Attack(Player player) {
+        player.curHealth -= attack;
     }
 
-    void ComeUp() {
-        FreezeAll(false);
-        agent.SetDestination(carPos.position);
-    }
-
-    public void AssignTask(Task newTask) {
-        curTask = newTask;
-        completion = TaskCompletion.NotStarted;
-    }
-
+    
     //-------------------------STATE----------------------------------
     void RunStateMachine() {
-        EnemyState newState = curState.CheckTransition();//先check再update
+        EnemyState newState = CheckTransition();//先check再update
         if (newState.type != curState.type) {
             curState.Exit();
             curState = newState;
@@ -81,18 +92,31 @@ public class Enemy : MonoBehaviour {
         curState.Update();
     }
 
+    protected virtual EnemyState CheckTransition() { return curState; }
+    /*EnemyState CheckTransition() {
+        if (anim.GetFloat("Speed") > 0) {
+            return new MoveState(this);
+        }
+        if (Vector3.Distance(Singleton<PlayerMovementControl>.Instance.transform.position, transform.position) <= attackDistance) {
+            return new AttackState(this);
+        }
+        if (this.anim.GetFloat("Speed") == 0) {
+             return new IdleState(this);
+         }
+    }*/
     public class IdleState : EnemyState {
 
         public IdleState(Enemy enemy) { context = enemy; type = EnemyStateEnum.Idle; }
         public override void Enter() {
             Debug.Log("enter idle state");
+
         }
 
         public override void Exit() {
         }
         public override void Update() {
             //执行Task
-            if (context.completion == TaskCompletion.NotStarted) {
+            /*if (context.completion == TaskCompletion.NotStarted) {
                 switch (context.curTask) {
                     case Task.ComeUp:
                         context.ComeUp();
@@ -122,51 +146,51 @@ public class Enemy : MonoBehaviour {
                 context.curTask = Task.Idle;
                 context.completion = TaskCompletion.Finished;
                 //stop & resume
-                context.FreezeAll(true);
-            }
-        }
-        public override EnemyState CheckTransition() {
-            if (context.anim.GetFloat("Speed") > 0) {
-                return new MoveState(context);
-            }
-            
-            return this;
+                context.agent.enabled = false;
+            }*/
         }
     }
 
     public class MoveState : EnemyState {
+        Vector3 target;
+        NavMeshAgent agent;
 
         public MoveState(Enemy enemy) { context = enemy; type = EnemyStateEnum.Move; }
         public override void Enter() {
             Debug.Log("enter move state");
+            agent = context.agent;
+            target = agent.destination;
         }
         public override void Exit() {
         }
         public override void Update() {
         }
-        public override EnemyState CheckTransition() {
-            if (context.anim.GetFloat("Speed") == 0) {
+        /*public override EnemyState CheckTransition() {
+            if (agent.remainingDistance <= agent.stoppingDistance) {
                 return new IdleState(context);
             }
+            if (context.playerDistance <= context.attackDistance) {
+                return new AttackState(context);
+            }
             return this;
-        }
+        }*/
     }
 
     public class AttackState : EnemyState {
         public AttackState(Enemy enemy) { context = enemy; type = EnemyStateEnum.Attack; }
         public override void Update() {
             base.Update();
-            switch (context.curMood) {
-                case Mood.Normal:
-                    break;
-                case Mood.Frightened: break;
-                case Mood.Angry: break;
-            }
         }
 
-        public override EnemyState CheckTransition() {
-            return base.CheckTransition();
-        }
+        /*public override EnemyState CheckTransition() {
+            if (context.anim.GetFloat("Speed") == 0) {
+                return new IdleState(context);
+            }
+            if (context.playerDistance <= context.attackDistance) {
+                return new AttackState(context);
+            }
+            return this;
+        }*/
     }
 
     public class EnemyState {
@@ -176,7 +200,7 @@ public class Enemy : MonoBehaviour {
         public virtual void Enter() { }
         public virtual void Exit() { }
         public virtual void Update() { }
-        public virtual EnemyState CheckTransition() { return this; }
+        //public virtual EnemyState CheckTransition() { return this; }
     }
 
     //----------------ENUMS-------------------------------
@@ -188,14 +212,19 @@ public class Enemy : MonoBehaviour {
         Normal, Angry, Frightened
     }
 
-    public enum Personality {
-        EatMelon, Evil, Coward
+    public class Personality { public virtual EnemyStateEnum CheckTransition(Player player) { return EnemyStateEnum.Idle; } }
+
+    class EatMelonPersona : Personality {
+        public override EnemyStateEnum CheckTransition(Player player) { return EnemyStateEnum.Idle; }
     }
-    public enum Task {
+    class EvilPersona : Personality { }
+    class CowardPersona : Personality { }
+
+   /* public enum Task {
         NormalWaitForCar, BadWaitForCar, ComeUp, Idle
     }
     public enum TaskCompletion {
         NotStarted, Doing, Finished
-    }
+    }*/
 }
 
--- Assets/Scripts/Player/PlayerMovementControl.cs
+++ Assets/Scripts/Player/PlayerMovementControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerMovementControl : MonoBehaviour {
     public float speed = 5f;
     /// <summary>
     /// 倍速
