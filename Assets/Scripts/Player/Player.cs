c75b3e37f772d77e30d2d2a9ac277d8ada63b46c
MIDORI\ishimidori <awolfgang1219@gmail.com>
2017-08-28 14:30:47 +0800

    + 开枪粒子

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,45 +5,23 @@ using UnityEngine;
 public class Player : Character {
     private static Player _instance;
     UnityChanControlScriptWithRgidBody control;
+    //PlayerShoot shoot;
 
-    public GameObject bulletPrefab;
-    public float timeBetweenBullets = 0.15f;
-    public float shootRange = 100f;
-    float timer = 0f;
 
     void Awake () {
         AwakeBase();
         _instance = this;
         atk = 15f;
         def = 3f;
-        //rb = GetComponent<Rigidbody>();
-        //anim = GetComponent<Animator>();
-        Debug.Log(rb);
-        control = GetComponent<UnityChanControlScriptWithRgidBody>();
+        control = GetComponent<UnityChanControlScriptWithRgidBody>();        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        timer += Time.deltaTime;
-        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0) {
-            Shoot();
-        }
-	}
-
-    void Shoot() {
-        timer = 0f;
-        //GameObject bullet=Instantiate(bulletPrefab
-        Ray shootRay = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        Debug.DrawRay(shootRay.origin, shootRay.direction * 15f, Color.red);
-        RaycastHit shootHit;
-        if (Physics.Raycast(shootRay, out shootHit, 15f, LayerMask.GetMask("Enemy"))) {
-            Enemy enemy = shootHit.collider.GetComponent<Enemy>();
-            if (enemy != null) {
-                Attack(enemy/*, shootHit.point*/);
-            }
-        }
-
+    void Start() {
+        //shoot = GetComponentInChildren<PlayerShoot>();
     }
+	
+    void Update () {
+	}
+
 
     protected override void DeadTemplate() {
         control.enabled = false;
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+    public Player player;
+    public GameObject bulletPrefab;
+    public float timeBetweenBullets = 0.15f;
+    public float shootRange = 100f;
+    float timer = 0f;
+    public int maxBullet = 5;
+    public int curBullet;
+
+    //Ray shootRay = new Ray();                       // A ray from the gun end forwards.
+    RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
+    int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+    ParticleSystem gunParticles;                    // Reference to the particle system.
+    LineRenderer gunLine;                           // Reference to the line renderer.
+    AudioSource gunAudio;                           // Reference to the audio source.
+    Light gunLight;                                 // Reference to the light component.
+    //public Light faceLight;								// Duh
+    float effectsDisplayTime = 0.2f;   
+
+    void Awake() {
+        curBullet = maxBullet;
+
+        shootableMask = LayerMask.GetMask("Enemy");
+
+        // Set up the references.
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+		
+	}
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0) {
+            Shoot();
+        }
+        if (timer >= timeBetweenBullets * effectsDisplayTime) {
+            DisableEffects();
+        }
+	}
+
+    public void DisableEffects() {
+        gunLine.enabled = false;
+        //faceLight.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    void Shoot() {
+        if (curBullet > 0) {
+            timer = 0f;
+            gunAudio.Play();
+            gunLight.enabled = true;
+            //faceLight.enabled = true;
+
+            // Stop the particles from playing if they were, then start the particles.
+            gunParticles.Stop();
+            gunParticles.Play();
+
+            // Enable the line renderer and set it's first position to be the end of the gun.
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
+
+            //GameObject bullet=Instantiate(bulletPrefab
+            Ray shootRay = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            Debug.DrawRay(shootRay.origin, shootRay.direction * 15f, Color.red);
+            RaycastHit shootHit;
+            if (Physics.Raycast(shootRay, out shootHit, shootRange, shootableMask)) {
+                Enemy enemy = shootHit.collider.GetComponent<Enemy>();
+                if (enemy != null) {
+                    player.Attack(enemy/*, shootHit.point*/);
+                    gunLine.SetPosition(1, shootHit.point);
+                }
+            }
+            else {
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * shootRange);
+            }
+            curBullet--;
+        }
+        else
+            Debug.Log("no bullet");
+    }
+
+
+    public void Reload() {
+        curBullet = maxBullet;
+    }
+}
